cfd14e39b87a14e2b3e8e1c025f495512d384ef4
Duc Hieu Nguyen <nduchieu0707@gmail.com>
2026-03-15 22:59:28 +0700

    Enhance Entity class with position and interaction methods

--- W1_Tasks/Entity_Task/Entity.py
+++ W1_Tasks/Entity_Task/Entity.py
@@ -1,15 +1,29 @@
 class Entity():
-    def __init__(self, name):
+    def __init__(self, name, position):
         self.name = name
+        self.position = position
 
 class Player(Entity):
+    def __init__(self, name, position):
+        super().__init__(name, position)
+        self.damage = 10
     def interact(self):
-        return (f"Player {self.name} is fighting")
+        return (f"{self.name} is attacking with {self.damage}")
     
 class NPC(Entity):
+    def __init__(self, name, position):
+        super().__init__(name, position)
+        self.diaglogue = "Hello"
     def interact(self):
-        return (f"NPC {self.name} is talking")
+        return (f"{self.name} is talking {self.diaglogue}")
     
-class Object(Entity):
+class Objects(Entity):
+    def __init__(self, name, position):
+        super().__init__(name, position)
+        self.is_break = True
     def interact(self):
-        return (f"Object {self.name} is being used")
+        if self.is_break:
+            action = "can be break"
+        else:
+            action = "cannot break"
+        return (f"{self.name} is doing so {action}")
