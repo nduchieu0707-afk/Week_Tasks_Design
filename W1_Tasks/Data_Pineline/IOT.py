736493f6f59f83ccc8b09ec4ec0fc80bf0bc31ff
Duc Hieu Nguyen <nduchieu0707@gmail.com>
2026-03-15 22:58:34 +0700

    Refactor IoT device management and add encryption
    Refactored IoT device classes and added serialization, deserialization, encryption, and decryption functionalities.

--- W1_Tasks/Data_Pineline/IOT.py
+++ W1_Tasks/Data_Pineline/IOT.py
@@ -1,90 +1,147 @@
 import csv
-import json
-from datetime import datetime
+import os
+from cryptography.fernet import Fernet
 
-class IoTDevice:
-    def __init__(self, device_id, location):
-        self.device_id = device_id
+class IoTDevice():
+    def __init__(self, deviceId, location, data):
+        self.deviceId = deviceId
         self.location = location
-        self.timestamp = datetime.now()
-    
-    def __str__(self):
-        return f"{self.device_id} at {self.location}"
+        self.data = data
 
 class TemperatureSensor(IoTDevice):
-    def __init__(self, device_id, location, temperature):
-        super().__init__(device_id, location)
-        self.temperature = temperature
-        self.data = {"temperature": temperature}
+    def __init__(self, deviceId, location, data):
+        super().__init__(deviceId, location, data)
+        self.type = "Temperature"
 
 class HumiditySensor(IoTDevice):
-    def __init__(self, device_id, location, humidity):
-        super().__init__(device_id, location)
-        self.humidity = humidity
-        self.data = {"humidity": humidity}
+    def __init__(self, deviceId, location, data):
+        super().__init__(deviceId, location, data)
+        self.type = "Humidity"
 
 class MotionSensor(IoTDevice):
-    def __init__(self, device_id, location, motion_detected):
-        super().__init__(device_id, location)
-        self.motion_detected = motion_detected
-        self.data = {"motion_detected": motion_detected}
+    def __init__(self, deviceId, location, data):
+        super().__init__(deviceId, location, data)
+        self.type = "Motion"
 
-class CSVHandler:
-    @staticmethod
-    def save_to_csv(devices, filename="devices.csv"):
+def serialize(devices, filename="iot_data.csv"):
+    try:
         with open(filename, 'w', newline='') as f:
             writer = csv.writer(f)
-            writer.writerow(['type', 'id', 'location', 'data'])
-            
-            for device in devices:
-                writer.writerow([
-                    device.__class__.__name__,
-                    device.device_id,
-                    device.location,
-                    json.dumps(device.data)
-                ])
-        print(f"Saved {len(devices)} devices to {filename}")
-    
-    @staticmethod
-    def load_from_csv(filename="devices.csv"):
-        devices = []
-        with open(filename, 'r') as f:
-            reader = csv.DictReader(f)
-            for row in reader:
-                device_type = row['type']
-                device_id = row['id']
-                location = row['location']
-                data = json.loads(row['data'])
-                
-                if device_type == "TemperatureSensor":
-                    device = TemperatureSensor(device_id, location, data['temperature'])
-                elif device_type == "HumiditySensor":
-                    device = HumiditySensor(device_id, location, data['humidity'])
-                elif device_type == "MotionSensor":
-                    device = MotionSensor(device_id, location, data['motion_detected'])
-                else:
-                    continue
-                
-                devices.append(device)
-        print(f"Loaded {len(devices)} devices from {filename}")
-        return devices
+            writer.writerow(["Type", "DeviceID", "Location", "Data"])
+            for d in devices:
+                writer.writerow([d.type, d.deviceId, d.location, d.data])
+        print("successfully!")
+    except:
+        print("failed!")
 
-class SimpleEncryptor:
-    @staticmethod
-    def encrypt_file(input_file, output_file, key="secret"):
-        with open(input_file, 'rb') as f:
+def deserialize(filename="iot_data.csv"):
+    devices = []
+    try:
+        with open(filename, 'r') as f:
+            reader = csv.reader(f)
+            next(reader)
+            for row in reader:
+                if len(row) == 4:
+                    t, id, loc, data = row
+                    if t == "Temperature":
+                        devices.append(TemperatureSensor(id, loc, data))
+                    elif t == "Humidity":
+                        devices.append(HumiditySensor(id, loc, data))
+                    elif t == "Motion":
+                        devices.append(MotionSensor(id, loc, data))
+        print("successfully!")
+        return devices
+    except:
+        print("failed!")
+        return []
+
+def encrypt(filename="iot_data.csv"):
+    try:
+        key = Fernet.generate_key()
+        with open("key.key", 'wb') as f:
+            f.write(key)
+        
+        fernet = Fernet(key)
+        with open(filename, 'rb') as f:
             data = f.read()
         
-        key_bytes = key.encode()
-        encrypted = bytearray()
-        for i, byte in enumerate(data):
-            encrypted.append(byte ^ key_bytes[i % len(key_bytes)])
+        with open(filename + ".enc", 'wb') as f:
+            f.write(fernet.encrypt(data))
+        print("successfully!")
+    except:
+        print("failed!")
+
+def decrypt(filename="iot_data.csv.enc"):
+    try:
+        with open("key.key", 'rb') as f:
+            key = f.read()
         
-        with open(output_file, 'wb') as f:
-            f.write(encrypted)
-        print(f"Encrypted {input_file} to {output_file}")
+        fernet = Fernet(key)
+        with open(filename, 'rb') as f:
+            data = f.read()
+        
+        with open("iot_data_decrypted.csv", 'wb') as f:
+            f.write(fernet.decrypt(data))
+        print("successfully!")
+    except:
+        print("failed!")
+
+def main():
+    devices = []
     
-    @staticmethod
-    def decrypt_file(input_file, output_file, key="secret"):
-        SimpleEncryptor.encrypt_file(input_file, output_file, key)
-        print(f"Decrypted {input_file} to {output_file}")
+    while True:
+        print("\n=== IoT DEVICE MANAGEMENT ===")
+        print("1 - Add IoT Device")
+        print("2 - Serialize Data")
+        print("3 - Deserialize Data")
+        print("4 - Encrypt Data")
+        print("5 - Decrypt Data")
+        print("0 - Exit")
+        
+        try:
+            choice = int(input("Choice: "))
+            
+            if choice == 1:
+                print("\n1. Temperature | 2. Humidity | 3. Motion")
+                types = int(input("Type: "))
+                id = input("Device ID: ")
+                loc = input("Location: ")
+                data = input("Data: ")
+                
+                if types == 1:
+                    devices.append(TemperatureSensor(id, loc, data))
+                elif types == 2:
+                    devices.append(HumiditySensor(id, loc, data))
+                elif types == 3:
+                    devices.append(MotionSensor(id, loc, data))
+                print("Added!")
+            
+            elif choice == 2:
+                if devices:
+                    serialize(devices)
+                else:
+                    print("No devices!")
+            
+            elif choice == 3:
+                devices = deserialize()
+            
+            elif choice == 4:
+                encrypt()
+            
+            elif choice == 5:
+                decrypt()
+            
+            elif choice == 0:
+                print("Bye!")
+                break
+            
+            else:
+                print("Invalid choice!")
+                
+        except ValueError:
+            print("Enter a number!")
+        except:
+            print("Error!")
+
+if __name__ == "__main__":
+    main()
